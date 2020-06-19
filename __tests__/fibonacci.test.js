c68a64ca7e8f344cfccc87712107535917003511
Tristen Everett <spock214@gmail.com>
2020-06-19 10:48:52 -0700

    create second test to check function to filter an array to just numbers divisible by an inputed number

--- __tests__/fibonacci.test.js
+++ __tests__/fibonacci.test.js
@@ -1,9 +1,15 @@
 import { fibonacci } from './../src/even_fibonacci'
+import { divisorFilter } from './../src/even_fibonacci'
 
 describe('even_fibonacci', () => {
   test('should correctly create an array of fibonacci numbers', () => {
     const fibonacciArray = fibonacci(100);
-    console.log(fibonacciArray);
     expect(fibonacciArray).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
+  });
+
+  test('should correctly return an array of numbers divisible by inputted number from inputted array', () => {
+    const fibonacciArray = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+    const evenArray = divisorFilter(2, fibonacciArray);
+    expect (evenArray).toEqual([0, 2, 8, 34]);
   });
 });
