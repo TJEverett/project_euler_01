0c75d3f2c01862c8e18ae33f262604e691b88530
Tristen Everett <spock214@gmail.com>
2020-06-19 11:10:35 -0700

    create third test to check function to add numbers from an array together

--- __tests__/fibonacci.test.js
+++ __tests__/fibonacci.test.js
@@ -1,5 +1,6 @@
 import { fibonacci } from './../src/even_fibonacci'
 import { divisorFilter } from './../src/even_fibonacci'
+import { arrayAddition } from './../src/even_fibonacci'
 
 describe('even_fibonacci', () => {
   test('should correctly create an array of fibonacci numbers', () => {
@@ -10,6 +11,12 @@ describe('even_fibonacci', () => {
   test('should correctly return an array of numbers divisible by inputted number from inputted array', () => {
     const fibonacciArray = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
     const evenArray = divisorFilter(2, fibonacciArray);
-    expect (evenArray).toEqual([0, 2, 8, 34]);
+    expect(evenArray).toEqual([0, 2, 8, 34]);
+  });
+
+  test('should correctly add the numbers from an inputted array together', () => {
+    const evenArray = [0, 2, 8, 34];
+    const sum = arrayAddition(evenArray);
+    expect(sum).toEqual(44);
   });
 });
